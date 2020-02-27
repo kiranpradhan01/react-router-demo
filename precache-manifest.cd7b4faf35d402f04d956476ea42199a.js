b346673c92e2239fb80d89eb55f097644cd9030d
Kiran Pradhan <kpradhan@uw.edu>
2020-02-26 16:24:27 -0800

    Updates

---
+++ precache-manifest.cd7b4faf35d402f04d956476ea42199a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb874de32c009e9de456b8d601839de",
+    "url": "/react-router-demo/index.html"
+  },
+  {
+    "revision": "de7024cfad6b4968ff83",
+    "url": "/react-router-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b0213114db2b5099a9b3",
+    "url": "/react-router-demo/static/js/2.668ccad2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-router-demo/static/js/2.668ccad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7024cfad6b4968ff83",
+    "url": "/react-router-demo/static/js/main.6ad539f6.chunk.js"
+  },
+  {
+    "revision": "b80553a2984a8e7b8672",
+    "url": "/react-router-demo/static/js/runtime-main.611882a6.js"
+  }
+]);
